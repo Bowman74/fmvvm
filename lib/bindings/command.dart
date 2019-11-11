d9983cafc30a2890df90a6ddcfa64147098b8193
João Palma <joaowd@outlook.com>
2019-11-11 19:22:25 +0000

    Update command.dart (#22)
    * Update command.dart
    
    * Update command.dart

--- lib/bindings/command.dart
+++ lib/bindings/command.dart
@@ -4,12 +4,18 @@ part of fmvvm.bindings;
 ///
 /// A command object should be returned by a property get statement.
 class Command {
-  Command(Function function) {
+  Command(Function function, [Function canExecuteFunction]) {
     _function = function;
+    _canExecuteFunction = canExecuteFunction ?? () => true;
   }
-
+  
   Function _function;
+  Function _canExecuteFunction;
 
   /// Execute's the function on the BindableBase object.
   Function get execute => _function;
+  /// Execute's the function on the BindableBase object if allowed.
+  Function get executeIf => _canExecuteFunction() ? _function : (){};
+  /// Checks if function can be executed on the BindableBase object.
+  Function get canExecute => _canExecuteFunction;
 }
